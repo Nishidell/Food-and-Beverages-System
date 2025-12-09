90e5e62a0cb591361b323865b594b7e3aa66a329
Nishidell <166776404+Nishidell@users.noreply.github.com>
2025-12-09 12:32:18 +0800

    KITCHEN: (it is not tested yet) Implement active room retrieval and integrate with MenuPage for Room Dining

--- backend/src/routes/roomRoutes.js
+++ backend/src/routes/roomRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllRooms } from "../controllers/roomController.js";
+import { getAllRooms, getMyActiveRoom} from "../controllers/roomController.js";
+import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllRooms);
+router.get('/my-active-room', protect, getMyActiveRoom);
 
 export default router;
